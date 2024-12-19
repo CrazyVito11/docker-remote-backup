9ea58c9b4d5626a8030686a9b835d6c5064a01bc
CrazyVito11 <31567466+CrazyVito11@users.noreply.github.com>
2024-12-19 01:44:09 +0100

    Added encryption support

--- docker/sync-now.sh
+++ docker/sync-now.sh
@@ -6,4 +6,4 @@ set -e
 bash /config/rclone-template/generate-rclone-conf.sh /config/rclone-template/rclone.conf > /config/rclone/rclone.conf
 
 # Start syncing
-rclone sync "/data" "remote_backup:$SFTP_HOST_DESTINATION_PATH" -v --metadata --create-empty-src-dirs --stats 60s --log-file="/logs/rclone.log"
+rclone sync "/data" "remote_backup:/" -v --metadata --create-empty-src-dirs --stats 60s --log-file="/logs/rclone.log"
